84400531840676e6e2ff6609b42c3678360ea4db
Siddharth Prabakar <siddharthp0212@gmail.com>
2025-12-04 19:25:14 -0500

    MidBlock, UpBlock, updated forward passes

--- models/model.py
+++ models/model.py
@@ -97,7 +97,11 @@ class DownBlock(nn.Module):
             self.resnet_blocks.append(resnet_block)
         self.resnet_blocks = nn.ModuleList(self.resnet_blocks)
 
-        self.attention_block = AttentionBlock(channels=out_channels, num_heads=num_heads)
+        self.attention_blocks = []
+        for i in range(num_layers):
+            attention_block = AttentionBlock(channels=out_channels, num_heads=num_heads)
+            self.attention_blocks.append(attention_block)
+        self.attention_blocks = nn.ModuleList(self.attention_blocks)
 
         self.down_sample_layer = nn.Identity()
         if self.down_sample:
@@ -107,7 +111,58 @@ class DownBlock(nn.Module):
         out = x
         for i in range(self.num_layers):
             out = self.resnet_blocks[i](out, t_embeddings)
+            out = self.attention_blocks[i](out)
         
-        out = self.attention_block(out)
+        skip_connection = out
         out = self.down_sample_layer(out)
+        return out, skip_connection
+
+class MidBlock(nn.Module):
+    def __init__(self, channels, t_emb_dim, num_heads=4, num_layers=1):
+        super().__init__()
+
+        self.resnet_1 = ResnetBlock(channels, channels, t_emb_dim)
+        self.attention = AttentionBlock(channels, num_heads)
+        self.resnet_2 = ResnetBlock(channels, channels, t_emb_dim)
+
+    def forward(self, x, t_embeddings):
+        out = x
+        out = self.resnet_1(out, t_embeddings)
+        out = self.attention(out)
+        out = self.resnet_2(out, t_embeddings)
+
+        return out
+    
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, t_emb_dim, up_sample=True, num_heads=4, num_layers=1):
+        super().__init__()
+        self.num_layers = num_layers
+        self.up_sample = up_sample
+
+        combined_channels = 2 * out_channels
+        self.resnet_blocks = []
+        for i in range(num_layers):
+            resnet_block = ResnetBlock(in_channels=combined_channels if i == 0 else out_channels, out_channels=out_channels, t_embedding_dim=t_emb_dim)
+            self.resnet_blocks.append(resnet_block)
+        self.resnet_blocks = nn.ModuleList(self.resnet_blocks)
+
+        self.attention_blocks = []
+        for i in range(num_layers):
+            attention_block = AttentionBlock(channels=out_channels, num_heads=num_heads)
+            self.attention_blocks.append(attention_block)
+        self.attention_blocks = nn.ModuleList(self.attention_blocks)
+
+        self.up_sample_layer = nn.Identity()
+        if self.up_sample:
+            self.up_sample_layer = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x, skip_connection, t_embeddings):
+        x = self.up_sample_layer(x)
+        x = torch.cat([x, skip_connection], dim=1)
+
+        out = x
+        for i in range(self.num_layers):
+            out = self.resnet_blocks[i](out, t_embeddings)
+            out = self.attention_blocks[i](out)
+        
         return out

b6df51dacf852249d95920fe1bdf58778ccff83d
Sean Lee <naeseel18@gmail.com>
2025-12-12 21:44:12 -0500

    Cleaned up for submission

--- generate_cifar_improved_baseline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --account=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=J
-#SBATCH --time=4:00:00
-
-source .venv/bin/activate
-python3 -u generate_cifar10.py --config config/cifar10_improved_baseline.json --generate_samples
--- train_cifar10.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --account=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=J
-#SBATCH --time=4:00:00
-
-source .venv/bin/activate
-python3 -u train_cifar10.py
